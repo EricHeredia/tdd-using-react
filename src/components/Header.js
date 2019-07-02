603c8662e8f79f75393cf9bb22698bb17c68deeb
EricHeredia <EricDavidHeredia@gmail.com>
2019-07-02 17:31:56 -0500

    added tests, organized files

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+//import logo from './logo.svg'
+
+function Header() {
+  return (
+    <div className="Header">
+      <p>This is the header.</p>
+    </div>
+  )
+}
+
+export default Header
