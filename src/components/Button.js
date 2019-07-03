bee6300b1ab9e9d945cede2fd62b53ff3a17af7e
EricHeredia <EricDavidHeredia@gmail.com>
2019-07-03 14:40:10 -0500

    Added button tests and text

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,9 @@ import './Button.css';
 class Button extends Component {
   render() {
     return (
-      <button className="location-button">Store</button>
+      <button className="location-button">
+        {this.props.location ? this.props.location : "All Locations"}
+      </button>
     ); 
   }
 }
--- src/components/__tests__/Button.test.js
+++ src/components/__tests__/Button.test.js
@@ -12,10 +12,46 @@ describe("Button", function() {
 
   it('renders without crashing', () => {
     let mountedButton = shallow(<Button />);
-  });
+  })
 
   it('renders a button', () => {
     let button = mountedButton.find('button');
     expect(button.length).toBe(1);
+  })
+})
+
+describe("When a location is passed to it, do", () => {
+  let mountedButton;
+  let props;
+
+  beforeEach(() => {
+    props = {
+      location: "Location1"
+    }
+
+    mountedButton = shallow(<Button {...props} />);
+  })
+
+  it('displays the location', () => {
+    const locName = mountedButton.find('.location-button');
+    expect(locName.text()).toEqual('Location1');
+  })
+})
+
+describe("When a location is passed to it, do", () => {
+  let mountedButton;
+  let props;
+
+  beforeEach(() => {
+    props = {
+      location: undefined
+    }
+
+    mountedButton = shallow(<Button {...props} />);
+  })
+
+  it('displays the location', () => {
+    const locName = mountedButton.find('.location-button');
+    expect(locName.text()).toEqual('All Locations');
   })
 })
