036ea24db3c180bb9906b4b4222069298f8a964e
EricHeredia <EricDavidHeredia@gmail.com>
2019-07-03 03:18:31 -0500

    added more components and tests

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Header from '../components/Header'
-//import logo from './logo.svg';
-import '../App.css';
+import StoreLocator from './StoreLocator'
+//import '../App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <StoreLocator />
     </div>
   );
 }
